6918eda0afbb84c8bb4741cd103034e7fb2c9222
VMolo4ke <85548094+VMolo4ke@users.noreply.github.com>
2024-06-20 23:05:20 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,89 @@
-let a = document.querySelector('.list');
-let b = document.querySelector('.list__item')
-let c = document.querySelectorAll('.list__item')[1]
-let d = document.querySelectorAll('.list__item')[2]
+let categories = document.querySelectorAll('.list__item');
 
-a.addEventListener('click', function() {
-    b.classList.add('activ1')
-    b.classList.remove('activate1')
-    c.classList.add('activ2')
-    c.classList.remove('activate2')
-    d.classList.add('activ3')
-    d.classList.remove('activate3')
+let previous_category_btn = document.querySelector('.list__next-btn');
+let next_category_btn = document.querySelector('.list__previous-btn');
+
+
+let list = [[0, 1, 2], [1, 2, 0], [2, 0, 1]]
+let current_category = 1;
+let current_direction = 0;
+let flag;
+
+next_category_btn.addEventListener('click', function() {
+    if (current_direction === 0) {
+        current_category = (current_category === 2) ? 0 : current_category + 1;
+        current_direction = 1;
+    }
+    if (flag === 0) {
+        current_category = (current_category === 0) ? 2 : current_category + 1;
+    }
+    animate({
+        duration: 400,
+        timing(timeFraction) {
+            return timeFraction;
+        },
+        draw(progress) {
+            categories[list[current_category][0]].style.left = progress * 220 + 'px';
+            categories[list[current_category][0]].style.zIndex = 0;
+            categories[list[current_category][0]].style.scale = 1;
+            categories[list[current_category][1]].style.left = 110 - (progress * 110) + 'px';
+            categories[list[current_category][1]].style.scale = 1.3 - (progress * 0.3);
+            categories[list[current_category][1]].style.zIndex = 1;
+            categories[list[current_category][2]].style.left = 220 - (progress * 110) + 'px';
+            categories[list[current_category][2]].style.scale = 1 + (progress * 0.3);
+            categories[list[current_category][2]].style.zIndex = 5;
+        }
+    });
+    current_category = (current_category === 2) ? 0 : current_category + 1;
+    flag = 1
 });
+
+
+previous_category_btn.addEventListener('click', function() {
+    if (current_direction === 0) {
+        current_category = (current_category === 0) ? 2 : current_category - 1;
+        current_direction = 1
+    }
+    if (flag === 1) {
+        current_category = (current_category === 0) ? 2 : current_category - 1;
+    }
+    animate({
+        duration: 500,
+        timing(timeFraction) {
+            return timeFraction;
+        },
+        draw(progress) {
+            categories[list[current_category][2]].style.left = 220 - (progress * 220) + 'px';
+            categories[list[current_category][2]].style.zIndex = 0;
+            categories[list[current_category][2]].style.scale = 1;
+            categories[list[current_category][1]].style.left = 110 + (progress * 110) + 'px';
+            categories[list[current_category][1]].style.scale = 1.3 - (progress * 0.3);
+            categories[list[current_category][1]].style.zIndex = 1;
+            categories[list[current_category][0]].style.left = progress * 110 + 'px';
+            categories[list[current_category][0]].style.scale = 1 + (progress * 0.3);
+            categories[list[current_category][0]].style.zIndex = 5;
+        }
+    });
+    current_category = (current_category === 0) ? 2 : current_category - 1;
+    flag = 0
+});
+
+
+function animate({duration, draw, timing}) {
+
+    let start = performance.now();
+
+    requestAnimationFrame(function animate(time) {
+        let timeFraction = (time - start) / duration;
+        if (timeFraction > 1) timeFraction = 1;
+
+        let progress = timing(timeFraction)
+
+        draw(progress);
+
+        if (timeFraction < 1) {
+            requestAnimationFrame(animate);
+        }
+
+    });
+}
